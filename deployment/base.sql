fd1e1e7e9c792fed0eab32a00b860f819c76b146
MoneroOcean <support@moneroocean.stream>
2026-04-13 22:26:44 +0000

    Simplify pool to pplns and xmr-only paths

--- deployment/base.sql
+++ deployment/base.sql
@@ -14,11 +14,10 @@ CREATE TABLE `balance` (
   `payment_address` varchar(128) DEFAULT NULL,
   `payment_id` varchar(128) DEFAULT NULL,
   `pool_type` varchar(64) DEFAULT NULL,
-  `bitcoin` tinyint(1) DEFAULT NULL,
   `amount` bigint(26) DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `balance_id_uindex` (`id`),
-  UNIQUE KEY `balance_payment_address_pool_type_bitcoin_payment_id_uindex` (`payment_address`,`pool_type`,`bitcoin`,`payment_id`),
+  UNIQUE KEY `balance_payment_address_pool_type_payment_id_uindex` (`payment_address`,`pool_type`,`payment_id`),
   KEY `balance_payment_address_payment_id_index` (`payment_address`,`payment_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE `paid_blocks` (
@@ -78,7 +77,6 @@ CREATE TABLE `payments` (
   `pool_type` varchar(64) DEFAULT NULL,
   `payment_address` varchar(125) DEFAULT NULL,
   `transaction_id` int(11) DEFAULT NULL COMMENT 'Transaction ID in the transactions table',
-  `bitcoin` tinyint(1) DEFAULT '0',
   `amount` bigint(20) DEFAULT NULL,
   `block_id` int(11) DEFAULT NULL,
   `payment_id` varchar(128) DEFAULT NULL,
@@ -129,71 +127,30 @@ CREATE TABLE `ports` (
   `miners` int(11) DEFAULT NULL,
   `ssl_port` tinyint(1) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE TABLE `shapeshiftTxn` (
-  `id` varchar(64) NOT NULL,
-  `address` varchar(128) DEFAULT NULL,
-  `paymentID` varchar(128) DEFAULT NULL,
-  `depositType` varchar(16) DEFAULT NULL,
-  `withdrawl` varchar(128) DEFAULT NULL,
-  `withdrawlType` varchar(16) DEFAULT NULL,
-  `returnAddress` varchar(128) DEFAULT NULL,
-  `returnAddressType` varchar(16) DEFAULT NULL,
-  `txnStatus` varchar(64) DEFAULT NULL,
-  `amountDeposited` bigint(26) DEFAULT NULL,
-  `amountSent` float DEFAULT NULL,
-  `transactionHash` varchar(128) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `shapeshiftTxn_id_uindex` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE `transactions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `bitcoin` tinyint(1) DEFAULT NULL,
   `address` varchar(128) DEFAULT NULL,
   `payment_id` varchar(128) DEFAULT NULL,
   `xmr_amt` bigint(26) DEFAULT NULL,
-  `btc_amt` bigint(26) DEFAULT NULL,
   `transaction_hash` varchar(128) DEFAULT NULL,
   `submitted_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `mixin` int(11) DEFAULT NULL,
   `fees` bigint(26) DEFAULT NULL,
   `payees` int(11) DEFAULT NULL,
-  `exchange_rate` bigint(26) DEFAULT NULL,
-  `confirmed` tinyint(1) DEFAULT NULL,
-  `confirmed_time` timestamp NULL DEFAULT NULL,
-  `exchange_name` varchar(64) DEFAULT NULL,
-  `exchange_txn_id` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `transactions_id_uindex` (`id`),
-  KEY `transactions_shapeshiftTxn_id_fk` (`exchange_txn_id`)
+  UNIQUE KEY `transactions_id_uindex` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(256) NOT NULL,
   `pass` varchar(64) DEFAULT NULL,
   `email` varchar(256) DEFAULT NULL,
-  `admin` tinyint(1) DEFAULT '0',
   `payout_threshold` bigint(16) DEFAULT '0',
   `enable_email` tinyint(1) DEFAULT '1',
   `payout_threshold_lock` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_id_uindex` (`id`),
   UNIQUE KEY `users_username_uindex` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE TABLE `xmrtoTxn` (
-  `id` varchar(64) NOT NULL,
-  `address` varchar(128) DEFAULT NULL,
-  `paymentID` varchar(128) DEFAULT NULL,
-  `depositType` varchar(16) DEFAULT NULL,
-  `withdrawl` varchar(128) DEFAULT NULL,
-  `withdrawlType` varchar(16) DEFAULT NULL,
-  `returnAddress` varchar(128) DEFAULT NULL,
-  `returnAddressType` varchar(16) DEFAULT NULL,
-  `txnStatus` varchar(64) DEFAULT NULL,
-  `amountDeposited` bigint(26) DEFAULT NULL,
-  `amountSent` float DEFAULT NULL,
-  `transactionHash` varchar(128) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `xmrtoTxn_id_uindex` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('pool', 'minerTimeout', '900', 'int', 'Length of time before a miner is flagged inactive.');
@@ -286,11 +243,8 @@ INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES 
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('pool', 'minDifficulty', '10000', 'int', 'Minimum difficulty for VarDiff');
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('pool', 'varDiffVariance', '20', 'int', 'Percentage out of the target time that difficulty changes');
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('pool', 'varDiffMaxChange', '125', 'int', 'Percentage amount that the difficulty may change');
-INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('payout', 'btcFee', '1.5', 'float', 'Fee charged for auto withdrawl via BTC');
-INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('payout', 'ppsFee', '6.5', 'float', 'Fee charged for usage of the PPS pool');
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('payout', 'pplnsFee', '.6', 'float', 'Fee charged for the usage of the PPLNS pool');
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('payout', 'propFee', '.7', 'float', 'Fee charged for the usage of the proportial pool');
-INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('payout', 'soloFee', '.4', 'float', 'Fee charged for usage of the solo mining pool');
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('payout', 'exchangeMin', '.1', 'float', 'Minimum XMR balance for payout to exchange/payment ID');
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('payout', 'walletMin', '.01', 'float', 'Minimum XMR balance for payout to personal wallet');
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('payout', 'defaultPay', '.1', 'float', 'Default XMR balance for payout');
@@ -302,15 +256,11 @@ INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES 
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('general', 'sigDivisor', '1000000000000', 'int', 'Divisor for turning coin into human readable amounts ');
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('payout', 'feesForTXN', '10', 'int', 'Amount of XMR that is left from the fees to pay miner fees.');
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('payout', 'maxTxnValue', '250', 'int', 'Maximum amount of XMR to send in a single transaction');
-INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('payout', 'shapeshiftPair', 'xmr_btc', 'string', 'Pair to use in all shapeshift lookups for auto BTC payout');
-INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('general', 'coinCode', 'XMR', 'string', 'Coincode to be loaded up w/ the shapeshift getcoins argument.');
-INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('payout', 'exchangeRate', '0', 'float', 'Current exchange rate');
-INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('payout', 'bestExchange', 'xmrto', 'string', 'Current best exchange');
+INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('general', 'coinCode', 'XMR', 'string', 'Coin code exposed by the API and payout layer.');
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('payout', 'mixIn', '10', 'int', 'Mixin count for coins that support such things.');
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('payout', 'ethWalletPass', '', 'string', 'Ethereum wallet password');
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('general', 'statsBufferLength', '1001', 'int', 'Number of items to be cached in the stats buffers.');
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('general', 'statsBufferHours', '72', 'int', 'Number of hours to be cached in the stats buffers.');
-INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('pps', 'enable', 'false', 'bool', 'Enable PPS or not');
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('pplns', 'shareMulti', '2', 'int', 'Multiply this times difficulty to set the N in PPLNS');
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('pplns', 'shareMultiLog', '3', 'int', 'How many times the difficulty of the current block do we keep in shares before clearing them out');
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('general', 'blockCleaner', 'true', 'bool', 'Enable the deletion of blocks or not.');
@@ -361,7 +311,6 @@ INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES 
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('general', 'testnet', 'false', 'bool', 'Does this pool use testnet?');
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('general', 'blockCleanWarning', '360', 'int', 'Blocks before longRunner cleaner module will start to warn.');
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('pplns', 'enable', 'true', 'bool', 'Enable PPLNS on the pool.');
-INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('solo', 'enable', 'true', 'bool', 'Enable SOLO mining on the pool');
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('payout', 'feeSlewAmount', '.0001', 'float', 'Amount to charge for the txn fee');
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('payout', 'feeSlewEnd', '4', 'float', 'Value at which txn fee amount drops to 0');
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('payout', 'rpcPasswordEnabled', 'false', 'bool', 'Does the wallet use a RPC password?');
@@ -379,7 +328,6 @@ INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES 
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('payout', 'priority', '1', 'int', 'Payout priority setting. 0 = use default (4x fee); 1 = low prio (1x fee)');
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('general', 'allowStuckPoolKill', 'false', 'bool', 'Allow to kill the pool in case of stuck block template');
 INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('general', 'dbSizeGB', '24', 'int', 'LMDB size in GBs');
-INSERT INTO pool.users (username, pass, email, admin, payout_threshold) VALUES ('Administrator', null, 'Password123', 1, 0);
 INSERT INTO pool.port_config (poolPort, difficulty, portDesc, portType, hidden, `ssl`) VALUES (3333, 1000, 'Low-End Hardware (Up to 30-40 h/s)', 'pplns', 0, 0);
 INSERT INTO pool.port_config (poolPort, difficulty, portDesc, portType, hidden, `ssl`) VALUES (5555, 5000, 'Medium-Range Hardware (Up to 160 h/s)', 'pplns', 0, 0);
 INSERT INTO pool.port_config (poolPort, difficulty, portDesc, portType, hidden, `ssl`) VALUES (7777, 10000, 'High-End Hardware (Anything else!)', 'pplns', 0, 0);
