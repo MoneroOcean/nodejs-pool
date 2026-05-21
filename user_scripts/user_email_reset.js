1f8bee62c9a7ca031c794975f8ff85e74a01a5ac
MoneroOcean <33983510+MoneroOcean@users.noreply.github.com>
2026-05-21 16:26:01 -0700

    Fix email reset script to preserve payout lock state

--- user_scripts/user_email_reset.js
+++ user_scripts/user_email_reset.js
@@ -5,7 +5,7 @@ const user = cli.arg("user", "Please specify user address to reset");
 
 cli.init(function() {
 	userDb.requireExistingUser(user, "User settings row does not exist").then(function () {
-		return userDb.runLoggedQuery("DELETE FROM users WHERE username = ?", [user], "DELETE FROM users WHERE username = " + user);
+		return userDb.runLoggedQuery("UPDATE users SET email = NULL, pass = NULL WHERE username = ?", [user], "UPDATE users SET email = NULL, pass = NULL WHERE username = " + user);
 	}).then(function () {
 		userDb.finish("Done. Please restart the miner or wait for it to reconnect and submit a valid share to set email/password again.");
 	});
