e2c228e4ea4b7200785a784ccbab05876d9ebe61
MoneroOcean <support@moneroocean.stream>
2026-06-16 11:32:50 -0700

    Use script_utils factory in user_bans_list
    Call the script_utils factory like the other user_/manage_ scripts
    instead of relying on the static createCli.init property. Behavior is
    identical since both resolve cli.init to initMini.init.

--- user_scripts/user_bans_list.js
+++ user_scripts/user_bans_list.js
@@ -1,6 +1,6 @@
 "use strict";
 const printBans = require("./user_bans_common.js");
-const cli = require("../script_utils.js");
+const cli = require("../script_utils.js")();
 
 cli.init(function() {
 	printBans(function () {
